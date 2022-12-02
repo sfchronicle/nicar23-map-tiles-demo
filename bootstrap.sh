f793881f5100ef66f4181f1981d57fe33fda340d
Evan Wagstaff <evanjwagstaff@gmail.com>
2022-12-01 16:02:26 -0800

    Do conditional for downlaods

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-# Get Planetiler file
-wget https://github.com/onthegomap/planetiler/releases/latest/download/planetiler.jar
-# Generate mbtiles using Planetiler
-java -Xmx1g -XX:MaxHeapFreeRatio=40 -jar planetiler.jar --area=tennessee --mbtiles=/data/tennessee.mbtiles --download --force
+# Get Planetiler file (if we don't have it)
+if [ ! -f /planetiler.jar ];then
+  wget https://github.com/onthegomap/planetiler/releases/latest/download/planetiler.jar
+fi
+# Generate mbtiles using Planetiler (if we don't have it)
+if [ ! -f /data/tennessee.mbtiles ];then
+  java -Xmx1g -XX:MaxHeapFreeRatio=40 -jar planetiler.jar --area=tennessee --mbtiles=/data/tennessee.mbtiles --download --force
+fi
 # Run tileserver-gl with the output file
 /usr/src/app/docker-entrypoint.sh --mbtiles /data/tennessee.mbtiles
 # echo "RUNNING NGINX3"
