78c0ae74d3cce9f2fb763183fb5c2971a42d7331
Evan Wagstaff <evanjwagstaff@gmail.com>
2023-01-20 16:37:03 -0800

    Configure super basic Gatsby page to display results

---
+++ demo-map/src/pages/finished.js
@@ -0,0 +1,47 @@
+import React, {useEffect} from 'react'
+import maplibregl from 'maplibre-gl'
+import 'maplibre-gl/dist/maplibre-gl.css'
+
+const IndexPage = () => {
+
+  useEffect(() => {
+    // Our own homemade style
+    const style = "https://nicar23-map-tiles.fly.dev/styles/blue_tennessee/style.json"
+
+    // Instantiate the map - heck yeah, it's free!
+    const map = new maplibregl.Map({
+      container: 'map', 
+      style: style,
+      center: [-86.7816, 36.1627], 
+      zoom: 12
+    });
+
+    map.on('load', () => {
+      map.addSource('subcounty', {
+        type: 'vector',
+        url: 'https://nicar23-map-tiles.fly.dev/data/tn_county_sub_tiger.json'
+      })
+      map.addLayer({
+        'id': 'subcounty',
+        'source': 'subcounty',
+        'source-layer': 'tn_county_sub_tiger',
+        'type': 'line',
+        'layout': {},
+        'paint': {
+          'line-color': 'papayawhip',
+          'line-width': 2,
+          'line-dasharray': [2, 2]
+        }
+      }, 'admin_sub')
+    })
+  })
+
+  return (
+    <main>
+      <h1>NICAR 2023: Open Source Vector Maps for Newsrooms</h1>
+      <div id='map' style={{'height': '500px'}}></div>
+    </main>
+  )
+}
+
+export default IndexPage
---
+++ demo-map/src/pages/index.js
@@ -0,0 +1,28 @@
+import React, {useEffect} from 'react'
+import maplibregl from 'maplibre-gl'
+import 'maplibre-gl/dist/maplibre-gl.css'
+
+const IndexPage = () => {
+
+  useEffect(() => {
+    // Our own homemade style
+    const style = ""
+
+    // Instantiate the map - heck yeah, it's free!
+    const map = new maplibregl.Map({
+      container: 'map', 
+      style: style,
+      center: [-86.7816, 36.1627], 
+      zoom: 12
+    });
+  })
+
+  return (
+    <main>
+      <h1>NICAR 2023: Open Source Vector Maps for Newsrooms</h1>
+      <div id='map' style={{'height': '500px', 'background': 'papayawhip'}}></div>
+    </main>
+  )
+}
+
+export default IndexPage
